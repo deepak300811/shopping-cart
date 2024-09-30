7d06caad45eb80b7ecd25d0e1a2351f6923c2e17
Deepak <deepakkumar481@ninjacart.com>
2024-10-01 00:37:39 +0530

    introduced a hook

--- src/Layout/MainContainer.tsx
+++ src/Layout/MainContainer.tsx
@@ -1,85 +1,16 @@
-import { collection, getDocs } from "firebase/firestore";
-import { useContext } from "react";
-import { useEffect } from "react";
 import ProductCard from "../Components/ProductCard";
-import { db } from "../firebase"; // Assuming this is where you configure Firestore
-import { App } from "../store/Context";
-import {
-  setProductList,
-  handleProductSelection,
-  handleRemoveOneProduct,
-  handleRemoveProductCompletely,
-  startLoader,
-  stopLoader,
-  setError,
-  unsetError,
-} from "../store/actionCreators";
 import { useCallback } from "react";
 import { NavList, Product } from "../types";
 import Home from "../icons/Home";
-import { useState } from "react";
 import AddressPage from "../Pages/Address";
 import Address from "../icons/Address";
 import CartWithNumer from "../Components/Cart";
 import CheckoutPage from "../Pages/Checkout";
 import Loader from "../Components/Loader";
 import ErrorModal from "../Components/ErrorModal";
-import { generic_error } from "../constants";
+import { useMainContainerHook } from "../hooks/useMainContainer";
 const MainContainer = () => {
-  const { state, dispatch } = useContext(App);
-  const [selectedPage, setSelectedPage] = useState("HOMEPAGE");
-
-  const fetchProducts = useCallback(async () => {
-    try {
-      dispatch(startLoader());
-      const productCollection = collection(db, "products");
-      const productSnapshot = await getDocs(productCollection); // Fetch data one time
-      let index = 0;
-      const productList = productSnapshot.docs.map((doc) => ({
-        id: doc.id,
-        quantity: 0,
-        indexNumber: index++,
-        name: doc.data().name,
-        originalPrice: doc.data().originalPrice,
-        photoLink: doc.data().photoLink,
-        description: doc.data().description,
-        stock: doc.data().stock,
-        discountedPrice: doc.data().discountedPrice,
-      }));
-      console.log("Fetched productList=", productList);
-      dispatch(setProductList(productList));
-    } catch (error) {
-      dispatch(stopLoader());
-      dispatch(
-        setError(generic_error, () => {
-          dispatch(unsetError());
-          fetchProducts();
-        })
-      );
-      console.error("Error fetching products: ", error);
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchProducts(); // Call the fetch function
-  }, []); // Empty dependency array for one-time fetch
-
-  const handleButtonClick = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
-    const target = e.target as HTMLElement; // Explicitly cast target to HTMLElement
-    const itemClickedId = target.getAttribute("data-index");
-    console.log("itemClickedId=", itemClickedId);
-    if (itemClickedId) {
-      dispatch(handleProductSelection(itemClickedId));
-    }
-  }, []);
-  console.log("state=", state);
-  const handleProductRemoval = (index: number, removalType: string) => {
-    if (removalType === "ONE") {
-      dispatch(handleRemoveOneProduct(index));
-    } else {
-      dispatch(handleRemoveProductCompletely(index));
-    }
-  };
+  const { state, selectedPage, setSelectedPage, handleButtonClick, handleProductRemoval } = useMainContainerHook();
 
   const getNavBar = useCallback(() => {
     let navItems: NavList = [];
--- src/hooks/useMainContainer.ts
+++ src/hooks/useMainContainer.ts
@@ -1,5 +1,5 @@
-import { useCallback, useContext, useEffect, useState, useMemo } from "react";
 import { collection, getDocs } from "firebase/firestore";
+import { useContext, useEffect, useCallback, useState } from "react";
 import { db } from "../firebase";
 import { App } from "../store/Context";
 import {
@@ -12,115 +12,63 @@ import {
   setError,
   unsetError,
 } from "../store/actionCreators";
-import { NavList, Product } from "../types";
 import { generic_error } from "../constants";
 
-const useMainContainer = () => {
-  const { state, dispatch } = useContext(App);
-  const [selectedPage, setSelectedPage] = useState("HOMEPAGE");
-
-  const fetchProducts = async () => {
-    try {
-      dispatch(startLoader());
-      const productCollection = collection(db, "products");
-      const productSnapshot = await getDocs(productCollection);
-      const productList = productSnapshot.docs.map((doc, index) => ({
-        id: doc.id,
-        quantity: 0,
-        indexNumber: index,
-        name: doc.data().name,
-        originalPrice: doc.data().originalPrice,
-        photoLink: doc.data().photoLink,
-        description: doc.data().description,
-        stock: doc.data().stock,
-        discountedPrice: doc.data().discountedPrice,
-      }));
-      dispatch(setProductList(productList));
-    } catch (error) {
-      dispatch(stopLoader());
-      dispatch(
-        setError(generic_error, () => {
-          dispatch(unsetError());
-          fetchProducts();
-        })
-      );
-    }
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  const handleButtonClick = useCallback(
-    (e: React.MouseEvent<HTMLDivElement>) => {
-      const target = e.target as HTMLElement;
+export const useMainContainerHook = () => {
+    const { state, dispatch } = useContext(App);
+    const [selectedPage, setSelectedPage] = useState("HOMEPAGE");
+  
+    const fetchProducts = useCallback(async () => {
+      try {
+        dispatch(startLoader());
+        const productCollection = collection(db, "products");
+        const productSnapshot = await getDocs(productCollection); // Fetch data one time
+        let index = 0;
+        const productList = productSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          quantity: 0,
+          indexNumber: index++,
+          name: doc.data().name,
+          originalPrice: doc.data().originalPrice,
+          photoLink: doc.data().photoLink,
+          description: doc.data().description,
+          stock: doc.data().stock,
+          discountedPrice: doc.data().discountedPrice,
+        }));
+        console.log("Fetched productList=", productList);
+        dispatch(setProductList(productList));
+      } catch (error) {
+        dispatch(stopLoader());
+        dispatch(
+          setError(generic_error, () => {
+            dispatch(unsetError());
+            fetchProducts();
+          })
+        );
+        console.error("Error fetching products: ", error);
+      }
+    }, []);
+  
+    useEffect(() => {
+      fetchProducts(); // Call the fetch function
+    }, []); // Empty dependency array for one-time fetch
+  
+    const handleButtonClick = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+      const target = e.target as HTMLElement; // Explicitly cast target to HTMLElement
       const itemClickedId = target.getAttribute("data-index");
+      console.log("itemClickedId=", itemClickedId);
       if (itemClickedId) {
         dispatch(handleProductSelection(itemClickedId));
       }
-    },
-    [dispatch]
-  );
-
-  const handleProductRemoval = (index: number, removalType: string) => {
-    if (removalType === "ONE") {
-      dispatch(handleRemoveOneProduct(index));
-    } else {
-      dispatch(handleRemoveProductCompletely(index));
-    }
-  };
-
-  const getNavBarItems = useMemo((): NavList => {
-    let navItems: NavList = [];
-    const commonNavItems:NavList = [
-      {
-        linkName: "Home",
-        icon: null,
-        isSelected: false,
-        handleClick: () => setSelectedPage("HOMEPAGE"),
-      },
-      {
-        linkName: "Address",
-        icon: null,
-        isSelected: false,
-        handleClick: () => setSelectedPage("ADDRESS"),
-      },
-    ];
-
-    if (selectedPage === "HOMEPAGE" && state.totalNoOfSelectedProdcuts && state.selectedAddressIndex > -1) {
-      navItems = [
-        ...commonNavItems,
-        {
-          linkName: "Checkout",
-          icon: null,
-          isSelected: false,
-          handleClick: () => setSelectedPage("CHECKOUT"),
-        },
-      ];
-    } else if (selectedPage === "HOMEPAGE" && state.totalNoOfSelectedProdcuts) {
-      navItems = [
-        {
-          linkName: "Continue",
-          icon: null,
-          isSelected: false,
-          isButton: true,
-          handleClick: () => setSelectedPage("ADDRESS"),
-        },
-      ];
-    } else if (selectedPage === "CHECKOUT") {
-      navItems = [
-        ...commonNavItems,
-        {
-          linkName: "Checkout",
-          icon: null,
-          isSelected: false,
-          handleClick: () => setSelectedPage("CHECKOUT"),
-        },
-      ];
-    }
-
-    return navItems;
-  }, [selectedPage, state.totalNoOfSelectedProdcuts, state.selectedAddressIndex]);
+    }, []);
+    console.log("state=", state);
+    const handleProductRemoval = (index: number, removalType: string) => {
+      if (removalType === "ONE") {
+        dispatch(handleRemoveOneProduct(index));
+      } else {
+        dispatch(handleRemoveProductCompletely(index));
+      }
+    };
 
   return {
     state,
@@ -128,8 +76,6 @@ const useMainContainer = () => {
     setSelectedPage,
     handleButtonClick,
     handleProductRemoval,
-    getNavBarItems,
+    
   };
 };
-
-export default useMainContainer;
